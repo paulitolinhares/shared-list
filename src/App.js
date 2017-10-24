fc751f17d39aab6d38e97e17edecc50aee979f85
Paulo Linhares <paulitolinhares@gmail.com>
2017-10-24 16:12:54 -0300

    small changes to trigger deployment

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">Hello, jovem!</h1>
     </header>
-    <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
   </div>
 );
 
