d0a32247ed00846101b82ac69b676d49cb7fe98e
Paulo Linhares <paulitolinhares@gmail.com>
2017-10-31 15:35:24 -0300

    fixing linting problems

--- src/components/Lists/index.js
+++ src/components/Lists/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const renderLists = (lists, handleSelection) =>
   lists.map(list =>
-    <li> <span onClick={() => handleSelection(list)}>{list.title}</span> </li>
-  );
+    <li> <button href="#" onClick={() => handleSelection(list)}>{list.title}</button> </li>);
 
-const Lists = ({ lists, handleSelection }) => {
-  return (
-    <aside className="column is-2">
-      <h2>Lists</h2>
-      <ul>
-        { renderLists(lists, handleSelection) }
-      </ul>
-    </aside>
-  );
+const Lists = ({ lists, handleSelection }) => (
+  <aside className="column is-2">
+    <h2>Lists</h2>
+    <ul>
+      { renderLists(lists, handleSelection) }
+    </ul>
+  </aside>
+);
+
+Lists.propTypes = {
+  lists: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+  })),
+  handleSelection: PropTypes.func,
+};
+
+Lists.defaultProps = {
+  lists: [],
+  handleSelection: () => { console.log('no handleSelection provided'); },
 };
 
 export default Lists;
